91bda55088c71d8fff88b3c6ee9860f7bb288a74
DieppDiepp <23521041@gm.uit.edu.vn>
2025-06-08 11:55:13 +0700

    Create folder powerBI_visualization and sql_sever_pre_processing, and adding two example file

---
+++ it-job-trend-analyzer/src/analysis_data/sql_sever_pre_processing/Trigger_clean_data.sql
@@ -0,0 +1,16 @@
+﻿-- Trigger để xóa liên đới từ JOB_DETAIL khi xóa JOB
+CREATE TRIGGER trg_DeleteJobWithDetails
+ON JOB
+INSTEAD OF DELETE
+AS
+BEGIN
+    -- Xoá trước các dòng liên quan trong JOB_DETAIL
+    DELETE FROM JOB_DETAIL
+    WHERE JOB_ID IN (SELECT JOB_ID FROM DELETED);
+
+    -- Sau đó xoá chính dòng trong JOB
+    DELETE FROM JOB
+    WHERE JOB_ID IN (SELECT JOB_ID FROM DELETED);
+END;
+
+
